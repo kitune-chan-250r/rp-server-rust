62d726a598018287526bc0747cd52dc85470b5b2
kitune-chan-250r <nekoo.hisui@gmail.com>
2025-08-13 06:46:28 +0000

    actix-webでredisを使ったセッション管理を行えるように設定

--- rp-server/app/src/main.rs
+++ rp-server/app/src/main.rs
@@ -1,14 +1,18 @@
 extern crate env_logger;
 use actix_cors::Cors;
-use actix_web::{middleware::Logger, web, App, HttpServer};
+use actix_session::{storage::RedisSessionStore, SessionMiddleware};
+use actix_web::{cookie::Key, middleware::Logger, web, App, HttpServer};
 mod controller;
 use controller::rp_controller;
+use log::info;
 use model::collection_user_credential::CollectionUserCredential;
+use rand::Rng;
 mod model;
 mod service;
 use crate::model::collection_challenge::CollectionChallenge;
 use mongodb::{
     bson::doc,
+    change_stream::session,
     options::{ClientOptions, Credential, IndexOptions},
     Client, Collection, IndexModel,
 };
@@ -48,6 +52,26 @@ async fn create_user_credential_index(client: &Client) {
         .expect("Failed to create index on user_credential field");
 }
 
+/**
+ * session管理に用いるredisのセットアップ
+ */
+async fn redis_setup() -> RedisSessionStore {
+    // Redisへの接続文字列
+    let redis_uri = "redis://redis:6379";
+
+    // Redisセッションストアを初期化
+    // .await が必要なので、main関数内で初期化する
+    let redis_store = match RedisSessionStore::new(redis_uri).await {
+        Ok(store) => store,
+        Err(e) => {
+            // サーバー起動時にRedisに接続できない場合はパニックさせる
+            panic!("Failed to connect to Redis: {}", e);
+        }
+    };
+    info!("Connected to Redis");
+    redis_store
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // ログ出力の設定
@@ -66,6 +90,7 @@ async fn main() -> std::io::Result<()> {
         .build();
     client_options.credential = Some(default_cred);
     let client = Client::with_options(client_options).expect("Failed to connect to MongoDB");
+    info!("Connected to MongoDB");
     create_challenge_index(&client).await;
 
     let challenge_collection: Collection<CollectionChallenge> = client
@@ -74,6 +99,11 @@ async fn main() -> std::io::Result<()> {
     let user_credential_collection: Collection<CollectionUserCredential> = client
         .database(&DB_NAME)
         .collection(&USER_CREDENTIAL_COLLECTION_NAME);
+
+    // 秘密鍵の生成
+    let secret_key = Key::from(&rand::rng().random::<[u8; 64]>());
+    // セッション管理を行うredisのセットアップ
+    let redis_session_store = redis_setup().await;
 
     HttpServer::new(move || {
         App::new()
@@ -84,6 +114,10 @@ async fn main() -> std::io::Result<()> {
                     .allow_any_header()
                     .allow_any_method(),
             )
+            .wrap(SessionMiddleware::new(
+                redis_session_store.clone(),
+                secret_key.clone(),
+            ))
             // .app_data(web::Data::new(client.clone()))
             .app_data(web::Data::new(challenge_collection.clone()))
             .app_data(web::Data::new(user_credential_collection.clone()))
