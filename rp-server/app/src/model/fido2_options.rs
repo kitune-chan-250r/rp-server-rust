57cb52b748cf87efd6b7b9f8a80f7b90a12b304a
kitune-chan-250r <nekoo.hisui@gmail.com>
2025-08-22 16:56:09 +0000

    フィールドをパブリックに変更

--- rp-server/app/src/model/fido2_options.rs
+++ rp-server/app/src/model/fido2_options.rs
@@ -4,8 +4,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Default, Builder, Serialize, Clone, Deserialize)]
 pub struct Fido2Options {
     #[serde(rename = "relyingPartyId")]
-    relying_party_id: String,
-    challenge: String,
-    user_verification: String,
-    timeout: Option<i64>,
+    pub relying_party_id: String,
+    pub challenge: String,
+    pub user_verification: String,
+    pub timeout: Option<i64>,
 }
